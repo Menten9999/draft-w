60f4bea3e3a63cff5adb69605dc7053789e217ce
Menten9999 <haru.menten.michaelis@gmail.com>
2025-11-02 08:59:54 +0900

    weighbor

---
+++ select/team2/firebase-config.js
@@ -0,0 +1,9 @@
+    const firebaseConfig = {
+  apiKey: "AIzaSyBwlnFf-ocmnNq3ZvudKC1Okgak6thaE9o",
+  authDomain: "draft-59823.firebaseapp.com",
+  databaseURL: "https://draft-59823-default-rtdb.firebaseio.com",
+  projectId: "draft-59823",
+  storageBucket: "draft-59823.firebasestorage.app",
+  messagingSenderId: "500987145047",
+  appId: "1:500987145047:web:7e948b8ae4493f7e84ac12"
+    };
